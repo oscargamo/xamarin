3f9e3294416da041d8e5559090a7f0754491477f
oscargamo <gamooscar10@gmail.com>
2020-03-16 12:15:07 -0600

    Todo el proyecto

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xuiFormDesign1 = new XanderUI.XUIFormDesign();
+            this.xuiFormDesign1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // xuiFormDesign1
+            // 
+            this.xuiFormDesign1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.xuiFormDesign1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xuiFormDesign1.ExitApplication = true;
+            this.xuiFormDesign1.FormStyle = XanderUI.XUIFormDesign.Style.MacOS;
+            this.xuiFormDesign1.Location = new System.Drawing.Point(0, 0);
+            this.xuiFormDesign1.MaterialBackColor = System.Drawing.Color.DodgerBlue;
+            this.xuiFormDesign1.MaterialForeColor = System.Drawing.Color.White;
+            this.xuiFormDesign1.Name = "xuiFormDesign1";
+            this.xuiFormDesign1.ShowMaximize = true;
+            this.xuiFormDesign1.ShowMinimize = true;
+            this.xuiFormDesign1.Size = new System.Drawing.Size(986, 575);
+            this.xuiFormDesign1.TabIndex = 0;
+            this.xuiFormDesign1.TitleText = "Form Name";
+            // 
+            // xuiFormDesign1.WorkingArea
+            // 
+            this.xuiFormDesign1.WorkingArea.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(236)))), ((int)(((byte)(236)))));
+            this.xuiFormDesign1.WorkingArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.xuiFormDesign1.WorkingArea.Location = new System.Drawing.Point(0, 39);
+            this.xuiFormDesign1.WorkingArea.Name = "WorkingArea";
+            this.xuiFormDesign1.WorkingArea.Size = new System.Drawing.Size(986, 536);
+            this.xuiFormDesign1.WorkingArea.TabIndex = 0;
+            this.xuiFormDesign1.Paint += new System.Windows.Forms.PaintEventHandler(this.xuiFormDesign1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 575);
+            this.Controls.Add(this.xuiFormDesign1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.xuiFormDesign1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private XanderUI.XUIFormDesign xuiFormDesign1;
+    }
+}
+
